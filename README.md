e09c8250a1ef87e6a43b1e92cb9380afdf3d396b
tomtechstarter <149696023+tomtechstarter@users.noreply.github.com>
2024-02-12 12:55:19 +0100

    Initial template commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Express Template
+
+1. `npm init` --> Um Projekt zu intitalisieren
+2. `npm i express dotenv cors`Um Pakete zu installieren
+3. `npm i -D nodemon` für Dev dependencies
+4. `index.js` anlegen
+5. `.env` für Umgebungsvariablen erstellen
+6. `.gitinore` ausetzen für `node_modules`
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+
+// Zugriff auf Umgebungsvariablen
+const { PORT } = process.env;
+
+// Initialisierung von expres
+const app = express();
+// Use for development
+app.use(cors());
+
+app.get("/test", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/profile", (req, res) => {
+  res.json({ profile: { name: "Max" } });
+});
+
+// App hört im folgenden auf den Port, welcher über die Umgebungsvariable definiert ist
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
