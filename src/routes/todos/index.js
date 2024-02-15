aeb713bd4454faff8b96855de9652db3182df24e
SirKaktor <juliuststelldinger@web.de>
2024-02-15 12:21:32 +0100

    implemented GET /byuserid

--- src/routes/todos/index.js
+++ src/routes/todos/index.js
@@ -18,21 +18,49 @@ const todos = [
     isDone: false,
     dueDate: new Date("2024-03-03"),
   },
+  {
+    id: 3,
+    userId: 2,
+    task: "Tanzen",
+    isDone: false,
+    dueDate: new Date("2024-03-03"),
+  },
+  {
+    id: 4,
+    userId: 2,
+    task: "Auto fahren",
+    isDone: true,
+    dueDate: new Date("2024-03-03"),
+  },
 ];
 
 // GET REQUESTS
-// /v1/todos/byid
+// /v1/todos/bytodoid
 TodosRouter.get("/byid", (req, res) => {
   const todoId = req.query.todoId;
   if (!todoId) {
     res.status(StatusCodes.BAD_REQUEST).send(ReasonPhrases.BAD_REQUEST);
     return;
   }
-  res.status(StatusCodes.OK).send("Get Todo by id");
+  res.status(StatusCodes.OK).send("Get Todo by (todo) id");
 });
 
-TodosRouter.post("/byuserid", (req, res) => {
-  res.status(StatusCodes.OK).send("Get Todo by user id");
+// Alle Todos von einer UserId
+TodosRouter.get("/byuserid", (req, res) => {
+  const userId = req.body.userId;
+  //const userId = req.query.userId;
+
+  if (!userId) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .send(ReasonPhrases.BAD_REQUEST + " Keine userID");
+    return;
+  }
+
+  const userTodos = todos.filter((todo) => todo.userId === userId);
+
+  res.status(StatusCodes.OK).json(userTodos);
+  // res.status(StatusCodes.OK).send(JSON.stringify(userTodos)); //alternativ
 });
 
 // PUT REQUESTS
